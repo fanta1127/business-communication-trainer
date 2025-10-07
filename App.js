b5bafa86c532db059adffb87ed3ad48a606989bd
fanta <163804768+fanta1127@users.noreply.github.com>
2025-10-07 19:31:42 +0900

    chore: プロジェクトの最終調整

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/contexts/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </AuthProvider> 
+    </SafeAreaProvider>  
   );
 }
