1ac0b9f8203fec1509c11f1e8854914c1b967c6d
fanta <163804768+fanta1127@users.noreply.github.com>
2025-10-11 10:36:07 +0900

    [feat] SessionProviderをApp.jsに追加

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/contexts/AuthContext';
+import { SessionProvider } from './src/contexts/SessionContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <SessionProvider>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </SessionProvider>
       </AuthProvider> 
     </SafeAreaProvider>  
   );
